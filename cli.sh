512b04c79b65d9c8ea06c094e80483412a904c8b
imelon2 <imelon279@naver.com>
2024-06-20 14:43:45 +0900

    first commit

---
+++ cli.sh
@@ -0,0 +1,159 @@
+# .env 파일의 경로 설정
+env_file=".env"
+
+# 파일 존재 여부 확인
+if [ -f "$env_file" ]; then
+    source "$env_file"
+else
+    echo "Error: $env_file does not exist."
+    exit 1
+fi
+
+
+func_help()
+{
+    echo Usage:
+    echo "  ./cli.sh [command]"
+    echo
+    echo Available Commands:
+    echo "  execution, e       Run Geth Cli"
+    echo "  consensus, c       Run Prysm Cli"
+    echo "  blockscout, b      Run Blockscout Cli"
+}
+
+func_execution_help()
+{
+    echo Usage:
+    echo "  ./cli.sh execution [command]"
+    echo
+    echo Available Commands:
+    echo "  new-account, new        Create a new account (PATH:/data/keystore)"
+    echo "  list-account, list      Print summary of existing accounts"
+    echo "  init                    Bootstrap and initialize a new genesis block"
+    echo "  run                     Run go-ethereum Execution Layer Node"
+    echo "  attach, a               Start an interactive JavaScript environment (PATH:/data/geth.ipc)"
+    echo "  clean                   Remove blockchain and state databases (PATH:/data)"
+}
+
+func_consensus_help()
+{
+    echo Usage:
+    echo "  ./cli.sh consensus [command]"
+    echo
+    echo Available Commands:
+    echo "  generate-genesis, gg      Generate genesis.json (PATH: $GENESIS_ROOT)"
+    echo "  create-jwt-secret, jwt    Create JWT secret (PATH: $JWTSECRET_ROOT)"
+    echo "  run-beacon-chain, runb    Run Consensus Layer beacon Node"
+    echo "  run-validator, runv       Run Consensus Layer Validator"
+    echo "  clean                     Remove beacochain and validator databases"
+}
+
+func_blockscout_help()
+{
+    echo Usage:
+    echo "  ./cli.sh blockscout [command]"
+    echo
+    echo Available Commands:
+    echo "  run              Run Geth Node Explorer (RPC: geth:$GETH_HTTP_PORT)"
+}
+
+func_execution()
+{
+    case "$1" in
+        "new-account"|"new")
+            echo "Run go-ethereum account new -- 🚀"
+            docker compose run --rm geth account new --datadir $EXECUTION_ROOT/data --password $PASSWORD_ROOT
+            ;;
+        "list-account"|"list")
+            echo "Run go-ethereum account list -- 🚀"
+            docker compose run --rm geth account list --datadir $EXECUTION_ROOT/data 
+            ;;
+        "init")
+            echo "Run go-ethereum init genesis block -- 🚀"
+            docker compose run --rm geth init --datadir $EXECUTION_ROOT/data $GENESIS_ROOT
+            ;;
+        "run")
+            echo "Run Execution Layer go-ethereum Node -- 🚀"
+            docker compose -p $PROJECT_NAME up -d geth
+            ;;
+        "attach"| "a")
+            docker compose run --rm geth-scripts attach $EXECUTION_ROOT/data/geth.ipc
+            ;;
+        "clean")
+            echo "Clear go-ethereum DB & Genesis -- 🗑️"
+            rm -rf ./execution/data/geth
+            ;;
+        "help" | "-h")
+            func_execution_help
+            ;;
+        *)
+        func_execution_help
+        ;;
+    esac
+}
+
+func_consensus()
+{
+    case "$1" in
+        "generate-genesis" | "gg")
+            echo "Generate genesis.json -- 🚀"
+            docker compose run --rm create-beacon-chain-genesis
+            ;;
+        "create-jwt-secret" | "jwt")
+            echo "Create JWT secret -- 🚀"
+            docker compose run --rm create-jwt-secret
+            ;;
+        "run-beacon-chain" | "runb")
+            echo "Run Consensus Layer beacon Node -- 🚀"
+            docker compose -p $PROJECT_NAME up -d beacon-chain
+            ;;
+        "run-validator" | "runv")
+            echo "Run Consensus Layer Validator -- 🚀"
+            docker compose -p $PROJECT_NAME up -d validator
+            ;;
+        "clean")
+            echo "Clear Beacon data & validator Data -- 🗑️"
+            rm -Rf ./consensus/beacondata ./consensus/validatordata ./consensus/genesis.ssz
+            ;;
+        "help" | "-h")
+            func_consensus_help
+            ;;
+        *)
+        func_consensus_help
+        ;;
+    esac
+}
+
+func_blockscout()
+{
+    case "$1" in
+        "run")
+            echo "Run Geth Node Explorer -- 🚀"
+            docker compose -p $PROJECT_NAME up -d blockscout
+            ;;
+        "help" | "-h")
+            func_blockscout_help
+            ;;
+        *)
+        func_blockscout_help
+        ;;
+    esac
+}
+
+case "$1" in
+    "execution" | "e")
+        func_execution $2
+        ;;
+    "consensus" | "c")
+        func_consensus $2
+        ;;
+    "blockscout" | "b")
+        func_blockscout $2
+        ;;
+    "help" | "h")
+        func_help
+        ;;
+    *)
+    func_help
+    ;;
+esac
---
+++ readme.md
@@ -0,0 +1,123 @@
+# Dkargo Layer 1 Chain | Ethereum Proof-of-Stake Devnet
+본 Repo는 **Geth와 Prysm**을 활용하여 Ethereum POS Local Node를 실행하기 위한 docker-compose와 CLI Tool을 제공합니다.
+> **[Optional]** 선택에 따라 **blockscout**를 활용하여 실행된 Node의 Block Explorer를 같이 실행할 수 있습니다.
+
+</br>
+
+## genesis.json & config.yml
+- genesis.json : /execution/genesis.json
+- config.yml : /consensus/config.yml
+
+</br>
+
+
+## Using CLI Tool
+```` bash
+% ./cli.sh help
+
+Usage:
+  ./cli.sh [command]
+
+Available Commands:
+  execution, e       Run Geth Cli
+  consensus, c       Run Prysm Cli
+  blockscout, b      Run Blockscout Cli
+````
+본 Repo는 Ethereum POS의 **`execution layer(Geth)`** 와 **`consensus layer(Prysm)`** 를 실행하기 위한 명령어(CLI)를 제공합니다.
+> **[Optional]** Block Explorer(Blockscout)를 실행하기 위한 명령어(CLI)를 제공합니다.
+
+</br>
+
+### Execution Layer CLI
+```` bash
+% ./cli.sh execution help 
+
+Usage:
+  ./cli.sh execution [command]
+
+Available Commands:
+  new-account, new        Create a new account (PATH:/data/keystore)
+  list-account, list      Print summary of existing accounts
+  init                    Bootstrap and initialize a new genesis block
+  run                     Run go-ethereum Execution Layer Node
+  attach, a               Start an interactive JavaScript environment (PATH:/data/geth.ipc)
+  clean                   Remove blockchain and state databases (PATH:/data)
+````
+</br>
+
+
+### Consensus Layer CLI
+```` bash
+% ./cli.sh consensus help 
+
+Usage:
+  ./cli.sh consensus [command]
+
+Available Commands:
+  generate-genesis, gg      Generate genesis.json (PATH: /nwolrd_dev_pos/execution/genesis.json)
+  create-jwt-secret, jwt    Create JWT secret (PATH: /nwolrd_dev_pos/execution/jwtsecret)
+  run-beacon-chain, runb    Run Consensus Layer beacon Node
+  run-validator, runv       Run Consensus Layer Validator
+  clean                     Remove beacochain and validator databases
+````
+
+### Blockscout CLI
+```` bash
+% ./cli.sh blockscout help 
+
+Usage:
+  ./cli.sh blockscout [command]
+
+Available Commands:
+  run              Run Geth Node Explorer (RPC: geth:8545)
+````
+
+## Build Node
+``` bash
+# (1) Create Auth JWT
+./cli.sh consensus create-jwt-secret
+
+# (2) Create genesis.json
+./cli.sh consensus generate-genesis
+
+# (3) init genesis block
+./cli.sh execution init
+
+# (4) Run execution & consensus Node
+./cli.sh execution run
+./cli.sh consensus run-beacon-chain
+./cli.sh consensus run-validator
+
+# (Optional) Run Block Explorer
+./cli.sh blockscout run
+
+```
+
+</br>
+
+## Config
+- 본 과정은 `Deneb` Ethereum를 하드포크합니다.
+- 본 Repo는 Docker Image `ethereum/client-go:v1.14.5`을 고정합니다.
+- Validator Deposit Contract는 `0x4242424242424242424242424242424242424242`에 배포됩니다.
+- Network 및 Client에 적용되는 환경 변수는 `.env`에서 설정할 수 있습니다.
+- 기본적으로 모든 RPC Host는 `0.0.0.0`로 설정되어 있습니다.
+- Geth로 계정을 생성하는 경우, `./execution/password.txt` 파일을 통해 Keystore 비밀번호를 설정할 수 있습니다.
+- [Optional] blockscout는 기본적으로 `http://localhost:4001`에서 실행됩니다.
+
+</br>
+
+## FAQ / Common Issues
+- ``` bash
+  WARN [06-20|01:45:18.368] Post-merge network, but no beacon client seen. Please launch one to follow the chain!
+  ```
+  Execution Layer와 Consensus Layer가 연결되지 않은 경우 발생하는 경고(WARN) Log입니다.
+
+- ```bash
+  ERROR[06-20|01:45:24.534] Nil finalized block cannot evict old blobs
+  ```
+  첫번째 블록이 확정성을 얻지 못해서 발생하는 애러 입니다. Ethereum Mainnet 기준으로 128 블록 생성 후 확정성을 얻으면 자연스럽게 없어집니다.
+  ````
+  # https://github.com/ethereum/go-ethereum/blob/27008408a57c77e55d3630adb50c72b0a36abb32/core/txpool/blobpool/limbo.go#L115 
+  Just in case there's no final block yet (network not yet merged, weird restart, sethead, etc), fail gracefully.
+  ````
+  
